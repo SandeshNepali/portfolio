9637a4b81f513af49ca8c0a6a59e9f39b64695e4
Sandesh Nepali <snepali@ucdavic.edu>
2024-09-03 13:57:13 +0545

    set 3, 2024 - first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Box, Typography } from "@mui/material"
+
+
+function App() {
+  return (
+    <>
+      <Box>
+        <Typography>
+
+          Sandesh Portfolio
+        </Typography>
+      </Box>
+
+    </>
+  );
+}
+
+export default App;
